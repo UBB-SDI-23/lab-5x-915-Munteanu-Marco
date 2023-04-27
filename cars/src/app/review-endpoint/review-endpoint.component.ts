3aa7c7ae0cab3588b26dc785e8c6308efa4b52c2
Marco Munteanu <marcomunteanu02@gmail.com>
2023-04-27 14:58:10 +0300

    add filter

--- cars/src/app/review-endpoint/review-endpoint.component.ts
+++ cars/src/app/review-endpoint/review-endpoint.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Review } from './models/review';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, concatMap, filter, map, of, switchMap, tap } from 'rxjs';
 import { Page } from '../car-endpoint/models/page';
 import { ReviewService } from './review.service';
 import { Router } from '@angular/router';
@@ -15,6 +15,9 @@ export class ReviewEndpointComponent {
 
   currentPageNumber: number = 1;
   reviewPage$: Observable<Page<Review>> = this.reviewService.getReviewPage(this.currentPageNumber);
+  filteredReviewPage$!: Observable<Page<Review>>;
+
+  scoreInputValue!: number;
 
   constructor(private reviewService: ReviewService, private router: Router) {}
 
@@ -24,6 +27,16 @@ export class ReviewEndpointComponent {
 
   onAdd(): void {
     this.router.navigateByUrl("/add-review");
+  }
+
+  onSearch(): void {
+    this.filteredReviewPage$ = this.reviewService.getReviewPage(this.currentPageNumber).pipe(
+            switchMap((page) => {
+              page.content = page.content.filter((review: Review) => review.score > this.scoreInputValue);
+              return of(page);
+            }),
+          );
+    
   }
 
   ngOnInit(): void {
